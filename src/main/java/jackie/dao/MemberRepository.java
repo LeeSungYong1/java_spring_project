908bad60f965e81e9dff7502e6ef36a7c4439746
sung_yong_lee <thddidwn@sparkplus.co>
2025-09-16 09:30:47 +0900

    test 수정

--- src/main/java/jackie/dao/MemberRepository.java
+++ src/main/java/jackie/dao/MemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * DB연결을 위한 인터페이스 선언
- * ORM 필요한 JpaRepository 상속 받아서 처리 하는 인터페이스 생성
+ * ORM 필요한 JpaRepository 상속 받아서 처리 하는 인터페이스 생성1
  */
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 }
